4c699c099d7e9ce0d84f24ea9568223e1548f828
ZaksenCode <maksimsidorkin2006@mail.ru>
2025-02-06 15:48:09 +0200

    Fix string to namespace regex again again.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "me.zaksen"
-version = "1.0.11"
+version = "1.0.12"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/me/zaksen/skillify_core/api/util/extensions/StringExtensions.kt
+++ src/main/kotlin/me/zaksen/skillify_core/api/util/extensions/StringExtensions.kt
@@ -3,9 +3,6 @@ package me.zaksen.skillify_core.api.util.extensions
 import org.bukkit.NamespacedKey
 
 fun String.toNamespacedKey(): NamespacedKey {
-    if(!this.matches(Regex.fromLiteral("[a-zA-Z_]+:[a-zA-Z_]+"))) {
-        throw IllegalArgumentException("To create NamespacedKey string should match regex: '[a-zA-Z_]+:[a-zA-Z_]+'")
-    }
     val split = this.split(":")
 
     if(split.size != 2) {
